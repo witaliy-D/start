57822fe0a2653932937e6e8d86d877006ab8a925
witaliy-D <witaliy999@gmail.com>
2020-02-15 16:46:33 +0200

    fix husky

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,7 @@
 import gulp from 'gulp';
-//import server from 'browser-sync';
 
 require('require-dir')('./tasks');
 
-
-//gulp.task('images', gulp.series('cleanImgs', gulp.parallel('imgs', 'symbols', 'sprite', 'favicons')), done => {
-//server.reload();
-//done();
-//});
 
 gulp.task('default', gulp.series(
 	'clean',
@@ -15,7 +9,6 @@ gulp.task('default', gulp.series(
 	gulp.parallel('imgs', 'html', 'scss', 'scripts'),
 	'serve'
 ));
-
 
 gulp.task('prod', gulp.series(
 	'clean',
